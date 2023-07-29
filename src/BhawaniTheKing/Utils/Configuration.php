7acfebde24d0e636211657e9a55bcd532dbccef5
Bhawani Singh <89442822+BhawaniTheKing@users.noreply.github.com>
2023-07-29 16:45:24 +0530

    Create Configuration.php

---
+++ src/BhawaniTheKing/Utils/Configuration.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace BhawaniTheKing\Utils;
+
+final class Configuration
+{
+  
+    /** @var string $FullInvPoPUP */
+    public string $fullInvPopup;
+
+    /** @var string $Mode */
+    public string $mode;
+
+    /** @var array $AffectedWorlds */
+    public array $affectedWorlds;
+    
+}
